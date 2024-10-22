5a92644a047854d0b6b437df299780b3c54ef90c
govind156 <govindmadaan156@gmail.com>
2024-10-22 12:17:43 +0530

    modify '/' route

--- routes/staticroute.js
+++ routes/staticroute.js
@@ -1,12 +1,16 @@
 const express=require('express')
 const { urlmodel } = require('../model/urlmodel')
+const { restrictTo } = require('../middleware/auth')
 const app=express()
 const router=express.Router()
 
-router.get("/",async(req,res)=>{
-     if(!req.user)
-         return res.redirect("/login")
-        const allurl=await urlmodel.find({createdby:req.user._id})
+router.get("/",restrictTo(["normal","admin"]),async(req,res)=>{
+    let allurl=null;
+    if(req.user.roles === "admin"){
+        allurl=await urlmodel.find({})
+    }else{
+        allurl=await urlmodel.find({createdby:req.user._id})
+    }     
     // const allurl=await urlmodel.find({})
     return res.render('home',{
         urls:allurl,
@@ -20,4 +24,6 @@ router.get("/signup",async(req,res)=>{
 router.get("/login",async(req,res)=>{
     return res.render("login")
 })
+
+
 module.exports=router

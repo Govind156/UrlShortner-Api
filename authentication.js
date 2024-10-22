467ec52ff8c3b46c8049e3da98130d0d5263c73d
govind156 <govindmadaan156@gmail.com>
2024-10-22 12:20:09 +0530

    modify setuser function

--- authentication.js
+++ authentication.js
@@ -1,12 +1,39 @@
-const sessionIdToUserMap=new Map()
+//stateful authentication
+// const sessionIdToUserMap=new Map()
 
-function getuser(id){
-    return sessionIdToUserMap.get(id)
+// function getuser(id){
+//     return sessionIdToUserMap.get(id)
+// }
+// function setuser(id,user){
+//     sessionIdToUserMap.set(id,user);
+// }
+// module.exports={
+//     getuser,
+//     setuser,
+// }
+
+//stateless authentication
+const jwt=require("jsonwebtoken")
+const secret="ab@12ab"
+function setuser(user){
+    return jwt.sign({
+        _id:user._id,
+        email:user.email,
+        roles:user.roles,
+    },secret)
 }
-function setuser(id,user){
-    sessionIdToUserMap.set(id,user);
+
+function getuser(token){
+    if(!token)
+        return null;
+    try{
+        return jwt.verify(token,secret)
+
+    }catch(error){
+        return null;
+    }
 }
+
 module.exports={
-    getuser,
-    setuser,
+    getuser,setuser,
 }
